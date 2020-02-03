8e80df4640818bd33b1ce06e7472705c94038dcd
Karan Bhanot <bhanotkaran22@gmail.com>
2020-02-03 11:06:14 -0500

    Setup the setuptools for the package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sythetic_data_kb22",
+    version="0.0.1",
+    author="Karan Bhanot",
+    author_email="bhanotkaran22@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheRensselaerIDEA/synthetic_data",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+)
